04efe3a1675f3be3e40256a90bd31e533e7b1c80
SakamakiGenki <k024g1038@g.neec.ac.jp>
2025-04-22 16:35:15 +0900

    PG3_01-01

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include<stdio.h>
 
+template <typename T>
+
+T Min(T a, T b) 
+{
+	if (a < b) 
+	{
+		return (a);
+	} else {
+		return (b);
+	}
+	
+}
+
+template <>
+char Min<char>(char a, char b) {
+
+	printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñB");
+	return 0;
+}
+
 int main() {
 
-	printf("Hello World0001");
-
+	printf("%d\n", Min<int>(2, 0));
+	printf("%c\n", Min<char>('g','d'));
+	printf("%.1f\n", Min<float>(200.0f, 300.0f));
+	printf("%.1lf\n", Min<double>(200.0f, 300.0f));
 	return 0;
 }
